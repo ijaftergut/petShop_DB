799844a57448ce21011b314ecccf8c3d05d7c2ad
Isaac Aftergut <ijagut@vt.edu>
2023-09-18 19:07:02 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const pg = require('pg');
+const client = new pg.Client('postgres://localhost/pet_shop');
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+app.use(cors());
+
+app.get('/api/pets', async(req, res, next)=> {
+  try {
+    const SQL = `
+      SELECT *
+      FROM pets
+    `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+const setup = async()=> {
+  await client.connect();
+  console.log('connected to the database');
+  const SQL = `
+    DROP TABLE IF EXISTS pets;
+    CREATE TABLE pets(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20),
+      is_favorite BOOLEAN
+    );
+    INSERT INTO pets (name, is_favorite) VALUES ('pet 1', false);
+    INSERT INTO pets (name, is_favorite) VALUES ('pet 2', false);
+    INSERT INTO pets (name, is_favorite) VALUES ('pet 3', false);
+    INSERT INTO pets (name, is_favorite) VALUES ('pet 4', true);
+  `;
+  await client.query(SQL);
+  console.log('tables created and data seeded');
+
+  const port = process.env.PORT || 3300;
+  app.listen(port, ()=> {
+    console.log(`listening on port ${port}`);
+  });
+};
+
+setup();
